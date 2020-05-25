a77f07907bfccefcbe8a7e362e8eb04241fec1fb
HRDVV <hrd.candy.dev@gmail.com>
2020-05-25 18:35:34 +0800

    button组件

--- examples/src/views/test.md
+++ examples/src/views/test.md
@@ -1,9 +1,16 @@
-## demo
+## 普通按钮
 
 :::demo
 ```html
-
-<arc-button></arc-button>
-
+<arc-button round>默认按钮</arc-button>
+<arc-button type="primary" plain>默认按钮</arc-button>
+<arc-button type="primary" >默认按钮</arc-button>
+<arc-button type="warning" plain>默认按钮</arc-button>
+<arc-button type="warning">默认按钮</arc-button>
+<arc-button type="success" size="medium" plain>默认按钮</arc-button>
+<arc-button type="success" size="medium">默认按钮</arc-button>
+<arc-button type="info" size="small" plain>默认按钮</arc-button>
+<arc-button type="info" size="small">默认按钮</arc-button>
+<arc-button type="error" size="mini" round plain>默认按钮</arc-button>
 ```
 :::
--- packages/components/Button/src/button.vue
+++ packages/components/Button/src/button.vue
@@ -1,17 +1,49 @@
-<template>
-  <button class="arc-button">
-
-  </button>
-</template>
-
 <script>
 export default {
   name: 'ArcButton',
   props: {
+    // primary / success / warning / error / info / text
     type: {
-      type: String
-
+      type: String,
+      default: ''
+    },
+    // medium / small / mini
+    size: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    round: {
+      type: Boolean,
+      default: false
+    },
+    plain: {
+      type: Boolean,
+      default: false
     }
+  },
+  render (createElement) {
+    const h = createElement
+    return h('button', {
+      class: [
+        'arc-button',
+        { 'arc-button--default': !this.type },
+        this.type ? `arc-button--${this.type + (this.plain ? '-plain' : '')}` : '',
+        this.size ? `arc-button--${this.size}` : '',
+        this.round ? 'arc-button--circle' : ''
+      ],
+      attrs: {
+        disabled: this.disabled
+      },
+      on: {
+        click: e => {
+          this.$emit('click', e)
+        }
+      }
+    }, this.$slots.default)
   }
 }
 </script>
